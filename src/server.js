064b779594bf0ddd2390e3da227c8f59e853c805
emilk <emil.kall@live.com>
2025-09-17 15:29:40 +0300

    Project setup

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const notesRouter = require('./routes/notes')
+const PORT = process.env.PORT || 6969
+
+
+require('dotenv').config()
+const app = express()
+console.log(`Node.js ${process.version}`)
+
+app.use(express.json())
+
+app.use('/users', usersRouter)
+
+
+app.listen(PORT, () => {
+    try {
+        console.log(`Running on http://localhost:${PORT}`)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+    
+})
